8b668c8bb0001bad40dd7efa8512e8ede921d763
feilongjump <feilongjump@qq.com>
2021-07-02 16:26:51 +0800

    重构 Int64ToString

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/gorilla/mux"
 	"go-blog/pkg/logger"
 	"go-blog/pkg/route"
+	"go-blog/pkg/types"
 	"html/template"
 	"net/http"
 	"net/url"
@@ -65,18 +66,13 @@ func articlesShowHandler(w http.ResponseWriter, r *http.Request) {
 		tmpl, err := template.New("show.tmpl").
 			Funcs(template.FuncMap{
 				"RouteName2URL": route.Name2URL,
-				"Int64ToString": Int64ToString,
+				"Int64ToString": types.Int64ToString,
 			}).
 			ParseFiles("resources/views/articles/show.tmpl")
 		logger.LogError(err)
 
 		tmpl.Execute(w, article)
 	}
-}
-
-// Int64ToString 将 int64 转换为 string
-func Int64ToString(num int64) string {
-	return strconv.FormatInt(num, 10)
 }
 
 func articlesIndexHandler(w http.ResponseWriter, r *http.Request) {
