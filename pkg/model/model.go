533c427efe39ea1b2c5232519579cb1ef34516b3
feilongjump <feilongjump@qq.com>
2021-07-06 14:06:50 +0800

    集成 GORM

---
+++ pkg/model/model.go
@@ -0,0 +1,23 @@
+package model
+
+import (
+	"go-blog/pkg/logger"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDB() *gorm.DB {
+	var err error
+
+	config := mysql.New(mysql.Config{
+		DSN: "root:secret@tcp(127.0.0.1:3306)/go_blog?charset=utf8&parseTime=True&loc=Local",
+	})
+
+	DB, err = gorm.Open(config, &gorm.Config{})
+
+	logger.LogError(err)
+
+	return DB
+}
